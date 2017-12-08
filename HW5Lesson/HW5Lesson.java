081f7847201167b12f820391f23a2b3f7f664bfc
Sergey Zhurov <s.jourov@gmail.com>
2017-12-08 14:14:28 +0300

    Base homework

---
+++ HW5Lesson/HW5Lesson.java
@@ -0,0 +1,50 @@
+/**
+ * Java 1. Lesson 3. Homework
+ * 
+ * @ author Sergey Zhurov
+ * @ vertion dated Dec 7 2017
+ * @ GitHub link https://github.com/SergeyZhurov/Java-1-Homeworks.git
+ */
+
+class HW5Lesson {
+    public static void main(String[] args) {
+        Person[] person = new Person[5];
+        person[0] = new Person("Ivan", "Software developer", "8(131)265-9986", 80000, 25);
+        person[1] = new Person("Nick", "Marketing specialist", "8(131)459-8622", 80000, 37);
+        person[2] = new Person("George", "CIO", "8(131)459-8211", 120000, 46);
+        person[3] = new Person("Olga", "Human Resources", "8(131)167-8455", 90000, 35);
+        person[4] = new Person("Katy", "Security", "8(131)156-6477", 65000, 40);
+        System.out.println("Employees older than 40 :");
+        for (int i = 0; i < person.length; i++)
+            if (person[i].getAge() >= 40) {
+                System.out.println();
+                person[i].print();
+            }
+    }
+ }
+ 
+class Person {
+
+    private String name, job, email, tel;
+    private int salary, age;
+
+    Person(String name, String job, String tel, int salary, int age) {
+        this.name = name;
+        this.job = job;
+        this.tel = tel;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    void print() {
+        System.out.println("Employee : " + name);
+        System.out.println("Job      : " + job);
+        System.out.println("Tel      : " + tel);
+        System.out.println("Salary   : " + salary);
+        System.out.println("Age      : " + age);
+    }
+    
+    int getAge() {
+        return age;
+    }
+}
