1578ed3904cbfe2627c1155d4a3d8b86fbb2d63e
Sergey Zhurov <s.jourov@gmail.com>
2017-12-16 03:47:23 +0300

    Buttons now work + debugged the code

--- HW7Lesson/HW7Lesson.java
+++ HW7Lesson/HW7Lesson.java
@@ -2,48 +2,106 @@
  * Java 1. Lesson 6. Homework
  *
  * @author Sergey Zhurov
- * @version dated Dec 15, 2017
+ * @version dated Dec 16, 2017
  */
 
+import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
 class HW7Lesson {
+    static Cat[] cat = new Cat[5];
+    static Plate plate = new Plate(50);
 
     public static void main(String[] args) {
         Window window = new Window();
-        Cat[] cat = new Cat[5];
-        cat[0] = new Cat("Tisha", 15);
-        cat[1] = new Cat("Vaska", 25);
-        cat[2] = new Cat("Murzik", 10);
-        cat[3] = new Cat("Kisa", 15);
-        cat[4] = new Cat("Baton", 40);
-        Plate plate = new Plate(50);
+        int[] appetite = {15, 25, 10, 15, 40};
+        cat[0] = new Cat("Tisha", appetite[0]);
+        cat[1] = new Cat("Vaska", appetite[1]);
+        cat[2] = new Cat("Murzik", appetite[2]);
+        cat[3] = new Cat("Kisa", appetite[3]);
+        cat[4] = new Cat("Baton", appetite[4]);
+        printCats();
         System.out.println(plate);
-        Random rand = new Random();
-        for(int i = 0; i < cat.length; i++) {                                                               // Trying to feed cats as many times as there are cats
-            int fullCats = 0;                                                                               // Checking if all cats are full
-            for(int j = 0; j < cat.length; j++) if(cat[j].getFull()) fullCats++;                            //
-            if(fullCats == cat.length) break;                                                               //
+    }
+    // Makes all cats hungry
+    public static void makeHungry() {                                         
+        for (int i = 0; i < cat.length; i++) cat[i].setFull(false);
+    }
+    //Tries to feed random cats untill all cats are full or no cat can get full from eating anymore
+    public static void feedCats() {                                           
+        Random rand = new Random();                                             
+        int minHungryAppetite;
+        do {                                                    // feeding cats
+            minHungryAppetite = 0;                              // getting hungry cat with the minimum appetite
+            for (int i = 0; i < cat.length; i++) {
+                if(cat[i].getFull() == false){
+                    minHungryAppetite = cat[i].getAppetite();
+                    break;
+                }
+            }
+            for (int j = 1; j < cat.length; j++)                // end cycle if all cats are full or no cat can get full
+                if((cat[j].getAppetite() < minHungryAppetite) && (cat[j].getFull() == false))
+                    minHungryAppetite = cat[j].getAppetite();
+            int fullCats = 0;                                                           // Checking if all cats are full
+            for(int j = 0; j < cat.length; j++) if(cat[j].getFull()) fullCats++;        
+            if(minHungryAppetite > plate.getFood() || (fullCats == cat.length)) break;
             do {
-                int pick = rand.nextInt(cat.length);                                                        // Picking random cat
-                if(cat[pick].getFull()) continue;                                                           // If cat is full - trying to pick another one
+                int pick = rand.nextInt(cat.length);            // Picking random cat
+                if(cat[pick].getFull()) continue;               // If cat is full - trying to pick another one
                 else {
-                    cat[pick].eat(plate);
+                    cat[pick].eat(plate);                       // else feed the cat
                     break;
                 }
             }while(true);
-        }
+        }while(true);
+        HW7Lesson.printCats();
+        System.out.println(plate);
+    }
+// Prints all the information about cats
+    public static void printCats() {
         for(int i = 0; i < cat.length; i++)
-            System.out.println(cat[i].getName() + " is " + ((cat[i].getFull())? "full." : "hungry."));
+            System.out.println(cat[i].getName() + "(appetite: " + cat[i].getAppetite() + ")"
+                    + " is " + ((cat[i].getFull())? "full." : "hungry."));
     }
 }
-
+// Window class
 class Window extends JFrame {
     public Window() {
         setTitle("Feeding cats.");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setBounds(300, 300, 400, 400);
+        setBounds(300, 300, 420, 68);
+        JButton jbsFeed = new JButton("Feed cats");
+        JButton jbsAddFood = new JButton("Add food to plate");
+        JButton jbsMakeHungry = new JButton("Make cats hungry");
+        setLayout(null);
+        jbsFeed.setBounds(0, 0, 135, 30);
+        jbsAddFood.setBounds(135, 0, 135, 30);
+        jbsMakeHungry.setBounds(270, 0, 135, 30);
+        add(jbsFeed);
+        add(jbsAddFood);
+        add(jbsMakeHungry);
+        jbsFeed.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                HW7Lesson.feedCats();
+            }
+        });
+        jbsAddFood.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                HW7Lesson.plate.addFood();
+                System.out.println(HW7Lesson.plate);
+            }
+        });
+        jbsMakeHungry.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                HW7Lesson.makeHungry();
+                HW7Lesson.printCats();
+                System.out.println(HW7Lesson.plate);
+            }
+        });
         setVisible(true);
     }
 }
@@ -65,12 +123,25 @@ class Cat {
         }
     }
 
+    int getAppetite() {
+        return appetite;
+    }
+
     boolean getFull() {
         return full;
     }
-    
+
+    void setFull(boolean full) {
+        this.full = full;
+    }
+
     String getName() {
         return name;
+    }
+
+    public String toString() {
+        return name + "(appetite: " + appetite + ")"
+                + " is " + ((full)? "full." : "hungry.");
     }
 }
 
@@ -85,9 +156,10 @@ class Plate {
     int getFood() {
         return food;
     }
-    
-    void fillPlate() {
-        food = maxFood;
+
+    void addFood() {
+        if (food + 10 <= maxFood) food += 10;
+        else System.out.println("plate is full");
     }
 
     void decreaseFood(int food) {
