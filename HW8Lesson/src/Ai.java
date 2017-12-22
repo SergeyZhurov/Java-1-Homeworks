436ff098fd4edde923f195e148ea2c28c99b5020
Sergey Zhurov <s.jourov@gmail.com>
2017-12-22 13:53:42 +0300

    renamed Constants to proper names

--- HW8Lesson/src/Ai.java
+++ HW8Lesson/src/Ai.java
@@ -9,32 +9,32 @@
 import java.util.Random;
 
 public class Ai extends Player{
-    private int MAP_SIZE, WIN_ROW;
+    private int mapSize, winRow;
     private Random rand = new Random();
 
-    Ai(final int MAP_SIZE, final int WIN_ROW, final char DOT) {
+    Ai(final int mapSize, final int winRow, final char DOT) {
         super(DOT);
-        this.MAP_SIZE = MAP_SIZE;
-        this.WIN_ROW = WIN_ROW;
+        this.mapSize = mapSize;
+        this.winRow = winRow;
     }
 
     int[] turn(int[][] valueMap) {                                         // Returns coordinates in [2] array
         int maxPrice;
         int[] move = new int[2];
         outerloop:
-        for (maxPrice = WIN_ROW - 1; maxPrice > 0; maxPrice--)            // Determining value of best possible move
-            for (int i = 0; i < MAP_SIZE; i++)
-                for (int j = 0; j < MAP_SIZE; j++)
+        for (maxPrice = winRow - 1; maxPrice > 0; maxPrice--)            // Determining value of best possible move
+            for (int i = 0; i < mapSize; i++)
+                for (int j = 0; j < mapSize; j++)
                     if (valueMap[i][j] == maxPrice) break outerloop;
         int tempCounter = 0;
-        for (int i = 0; i < MAP_SIZE; i++)                                // Counting number of moves with best value
-            for (int j = 0; j < MAP_SIZE; j++)
+        for (int i = 0; i < mapSize; i++)                                // Counting number of moves with best value
+            for (int j = 0; j < mapSize; j++)
                 if (valueMap[i][j] == maxPrice) tempCounter++;
         int aiMove = rand.nextInt(tempCounter) + 1;                      // Picking random move with best value
         tempCounter = 0;
         outerloop:
-        for (int i = 0; i < MAP_SIZE; i++)                               // Looking for picked move
-            for (int j = 0; j < MAP_SIZE; j++) {
+        for (int i = 0; i < mapSize; i++)                               // Looking for picked move
+            for (int j = 0; j < mapSize; j++) {
                 if (valueMap[i][j] == maxPrice) tempCounter++;
                 if (tempCounter == aiMove) {
                     move[0] = i;                                        // Actual AI move
@@ -45,10 +45,10 @@ public class Ai extends Player{
         return move;
     }
     void setSize(int x) {
-        MAP_SIZE = x;
+        mapSize = x;
     }
 
     void setWinRow(int x) {
-        WIN_ROW = x;
+        winRow = x;
     }
 }
