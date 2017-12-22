9a614e1e2dbbc84f4f4ab0d9b551bed1f862f820
Sergey Zhurov <s.jourov@gmail.com>
2017-12-22 14:21:52 +0300

    changed JavaDoc comments

--- HW8Lesson/src/Window.java
+++ HW8Lesson/src/Window.java
@@ -1,8 +1,8 @@
 /**
- * Java 1. Lesson 6. Homework task 8
+ * Java 1. Lesson 8. Homework
  *
  * @ author Sergey Zhurov
- * @ vertion dated Dec 11 2017
+ * @ vertion dated Dec 22 2017
  * @ GitHub link https://github.com/SergeyZhurov/Java-1-Homeworks.git
  */
 
